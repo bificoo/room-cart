cb1e318ff017dc36a278671b165d3390f5e2b181
bificoo <bificoo@gmail.com>
2022-09-25 10:37:17 +0800

    Set production mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,74 +3,76 @@ const json5 = require('json5');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const devMode = process.env.NODE_ENV !== "production";
 
-module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, './src/index.ts'),
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, './dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(t|j)sx?$/,
-        use: ['babel-loader'],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(sa|sc|c)ss$/i,
-        use: [
-          devMode ? { loader: "style-loader"} : MiniCssExtractPlugin.loader,
-          { loader: "css-modules-typescript-loader"},
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-            },
-          },
-          { loader: "sass-loader"},
-        ],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.json5$/i,
-        type: 'json',
-        parser: {
-          parse: json5.parse,
-        },
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', ".css", ".scss"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'AsiaYo',
-    }),
-    new CleanWebpackPlugin(),
-  ],
-  devtool: 'inline-source-map',
-  devServer: {
-    static: path.resolve(__dirname, "./dist"),
-    compress: true,
-    port: 8080,
-    open: true,
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
+module.exports = (env, argv) => {
+  const devMode = argv.mode === "development"
+  return {
+    entry: path.resolve(__dirname, './src/index.ts'),
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, './dist'),
+      clean: true,
     },
-  },
-};
+    module: {
+      rules: [
+        {
+          test: /\.(t|j)sx?$/,
+          use: ['babel-loader'],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(sa|sc|c)ss$/i,
+          use: [
+            devMode ? { loader: "style-loader" } : MiniCssExtractPlugin.loader,
+            { loader: "css-modules-typescript-loader" },
+            {
+              loader: "css-loader",
+              options: {
+                importLoaders: 1,
+              },
+            },
+            { loader: "sass-loader" },
+          ],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.json5$/i,
+          type: 'json',
+          parser: {
+            parse: json5.parse,
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', ".css", ".scss"],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'AsiaYo',
+      }),
+      ...(devMode ? [] : [new MiniCssExtractPlugin()]),
+      new CleanWebpackPlugin(),
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+      static: path.resolve(__dirname, "./dist"),
+      compress: true,
+      port: 8080,
+      open: true,
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
+  }
+}
